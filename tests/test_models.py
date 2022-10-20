31844143e3b6f33b12caa343ac503b3ac304e568
Douglas Lowe <10961945+douglowe@users.noreply.github.com>
2022-10-20 13:27:33 +0100

    update tests for catchment datasets

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,31 +1,51 @@
 """Tests for statistics functions within the Model layer."""
 
-import numpy as np
-import numpy.testing as npt
+import pandas as pd
+import pandas.testing as pdt
+import datetime
 
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
-    from inflammation.models import daily_mean
+    from catchment.models import daily_mean
 
-    test_input = np.array([[0, 0],
-                           [0, 0],
-                           [0, 0]])
-    test_result = np.array([0, 0])
+    test_input = pd.DataFrame(
+                     data=[[0.0, 0.0],
+                           [0.0, 0.0],
+                           [0.0, 0.0]],
+                     index=[pd.to_datetime('2000-01-01 01:00'),
+                            pd.to_datetime('2000-01-01 02:00'),
+                            pd.to_datetime('2000-01-01 03:00')],
+                     columns=['A', 'B']
+    )
+    test_result = pd.DataFrame(
+                     data=[[0.0, 0.0]],
+                     index=[datetime.date(2000, 1, 1)],
+                     columns=['A', 'B']
+    )
 
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_mean(test_input), test_result)
+    # Need to use Pandas testing functions to compare arrays
+    pdt.assert_frame_equal(daily_mean(test_input), test_result)
 
 
 def test_daily_mean_integers():
     """Test that mean function works for an array of positive integers."""
-    from inflammation.models import daily_mean
+    from catchment.models import daily_mean
 
-    test_input = np.array([[1, 2],
+    test_input = pd.DataFrame(
+                     data=[[1, 2],
                            [3, 4],
-                           [5, 6]])
-    test_result = np.array([3, 4])
+                           [5, 6]],
+                     index=[pd.to_datetime('2000-01-01 01:00'),
+                            pd.to_datetime('2000-01-01 02:00'),
+                            pd.to_datetime('2000-01-01 03:00')],
+                     columns=['A', 'B']
+    )
+    test_result = pd.DataFrame(
+                     data=[[3.0, 4.0]],
+                     index=[datetime.date(2000, 1, 1)],
+                     columns=['A', 'B']
+    )
 
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_mean(test_input), test_result)
-
+    # Need to use Pandas testing functions to compare arrays
+    pdt.assert_frame_equal(daily_mean(test_input), test_result)
