4e204480202adec131d57e31f665f164c7e91517
Scott Archer-Nicholls <s.archer-nicholls@gmail.com>
2024-03-19 10:09:56 +0000

    Getting up to date

--- catchment/models.py
+++ catchment/models.py
@@ -9,6 +9,7 @@ time across all sites.
 
 import pandas as pd
 
+
 def read_variable_from_csv(filename):
     """Reads a named variable from a CSV file, and returns a
     pandas dataframe containing that variable. The CSV file must contain
@@ -21,23 +22,28 @@ def read_variable_from_csv(filename):
     """
     dataset = pd.read_csv(filename, usecols=['Date', 'Site', 'Rainfall (mm)'])
 
-    dataset = dataset.rename({'Date':'OldDate'}, axis='columns')
-    dataset['Date'] = [pd.to_datetime(x,dayfirst=True) for x in dataset['OldDate']]
+    dataset = dataset.rename({'Date': 'OldDate'}, axis='columns')
+    dataset['Date'] = [
+        pd.to_datetime(x, dayfirst=True) for x in dataset['OldDate']
+    ]
     dataset = dataset.drop('OldDate', axis='columns')
 
     newdataset = pd.DataFrame(index=dataset['Date'].unique())
 
     for site in dataset['Site'].unique():
-        newdataset[site] = dataset[dataset['Site'] == site].set_index('Date')["Rainfall (mm)"]
+        newdataset[site] = \
+            dataset[dataset['Site'] == site].set_index('Date')["Rainfall (mm)"]
 
     newdataset = newdataset.sort_index()
 
     return newdataset
 
+
 def daily_total(data):
     """Calculate the daily total of a 2D data array.
     Index must be np.datetime64 compatible format."""
     return data.groupby(data.index.date).sum()
+
 
 def daily_mean(data):
     """Calculate the daily mean of a 2D data array.
@@ -55,4 +61,3 @@ def daily_min(data):
     """Calculate the daily min of a 2D data array.
     Index must be np.datetime64 compatible format."""
     return data.groupby(data.index.date).min()
-
