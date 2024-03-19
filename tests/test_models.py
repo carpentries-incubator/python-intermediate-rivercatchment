0768bdd0200e3625a7938b7c470b72e9c7aca294
Tara Cunningham <tcc78128@gmail.com>
2024-03-19 11:31:21 +0000

    Add parametrisation for test daily main, min and max.

--- tests/test_models.py
+++ tests/test_models.py
@@ -5,57 +5,171 @@ import pandas.testing as pdt
 import datetime
 import pytest
 
+########## daily mean ##########
+@pytest.mark.parametrize(
+    "test_input, expected_output",
+    [
+        (
+            pd.DataFrame(
+                data=[[0.0, 0.0], [0.0, 0.0], [0.0, 0.0]],
+                index=[pd.to_datetime('2000-01-01 01:00'),
+                        pd.to_datetime('2000-01-01 02:00'),
+                        pd.to_datetime('2000-01-01 03:00')],
+                columns=['A', 'B']
+            ),
+            pd.DataFrame(
+               data=[[0.0, 0.0]],
+               index=[datetime.date(2000, 1, 1)],
+               columns=['A', 'B']
+            )
+        ),
+        (
+            pd.DataFrame(
+                data=[[1, 2], [3, 4], [5, 6]],
+                index=[pd.to_datetime('2000-01-01 01:00'),
+                        pd.to_datetime('2000-01-01 02:00'),
+                        pd.to_datetime('2000-01-01 03:00')],
+                columns=['A', 'B'],
+            ),
+            pd.DataFrame(
+                data=[[3.0, 4.0]],
+                index=[datetime.date(2000, 1, 1)],
+                columns=['A', 'B']
+            )
+        ),
+    ])
 
-def test_daily_mean_zeros():
-    """Test that mean function works for an array of zeros."""
+def test_daily_mean(test_input, expected_output):
+    """Test mean function works for array of zeroes and positive integers."""
     from catchment.models import daily_mean
+    pdt.assert_frame_equal(daily_mean(test_input), expected_output)
 
-    test_input = pd.DataFrame(
-                     data=[[0.0, 0.0],
-                           [0.0, 0.0],
-                           [0.0, 0.0]],
-                     index=[pd.to_datetime('2000-01-01 01:00'),
-                            pd.to_datetime('2000-01-01 02:00'),
-                            pd.to_datetime('2000-01-01 03:00')],
-                     columns=['A', 'B']
-    )
-    test_result = pd.DataFrame(
-                     data=[[0.0, 0.0]],
-                     index=[datetime.date(2000, 1, 1)],
-                     columns=['A', 'B']
-    )
+########## daily min ##########
+@pytest.mark.parametrize(
+    "test_input, expected_output",
+    [
+        (
+            pd.DataFrame(
+                data=[[0.0, 0.0], [0.0, 0.0], [0.0, 0.0]],
+                index=[pd.to_datetime('2000-01-01 01:00'),
+                        pd.to_datetime('2000-01-01 02:00'),
+                        pd.to_datetime('2000-01-01 03:00')],
+                columns=['A', 'B']
+            ),
+            pd.DataFrame(
+               data=[[0.0, 0.0]],
+               index=[datetime.date(2000, 1, 1)],
+               columns=['A', 'B']
+            )
+        ),
+        (
+            pd.DataFrame(
+                data=[[1, 2], [3, 4], [5, 6]],
+                index=[pd.to_datetime('2000-01-01 01:00'),
+                        pd.to_datetime('2000-01-01 02:00'),
+                        pd.to_datetime('2000-01-01 03:00')],
+                columns=['A', 'B'],
+            ),
+            pd.DataFrame(
+                data=[[1, 2]],
+                index=[datetime.date(2000, 1, 1)],
+                columns=['A', 'B']
+            )
+        ),
+    ])
 
-    # Need to use Pandas testing functions to compare arrays
-    pdt.assert_frame_equal(daily_mean(test_input), test_result)
-
-
-def test_daily_mean_integers():
-    """Test that mean function works for an array of positive integers."""
-    from catchment.models import daily_mean
-
-    test_input = pd.DataFrame(
-                     data=[[1, 2],
-                           [3, 4],
-                           [5, 6]],
-                     index=[pd.to_datetime('2000-01-01 01:00'),
-                            pd.to_datetime('2000-01-01 02:00'),
-                            pd.to_datetime('2000-01-01 03:00')],
-                     columns=['A', 'B']
-    )
-    test_result = pd.DataFrame(
-                     data=[[3.0, 4.0]],
-                     index=[datetime.date(2000, 1, 1)],
-                     columns=['A', 'B']
-    )
-
-    # Need to use Pandas testing functions to compare arrays
-    pdt.assert_frame_equal(daily_mean(test_input), test_result)
-
-
-def test_daily_min_python_list():
-    """Test for attributeError when passing a python list"""
+def test_daily_min(test_input, expected_output):
+    """Test mean function works for array of zeroes and positive integers."""
     from catchment.models import daily_min
+    pdt.assert_frame_equal(daily_min(test_input), expected_output)
 
-    with pytest.raises(AttributeError):
-        error_expected = daily_min([[3, 4, 7], [-3, 0, 5]])
+########## daily_max ##########
+@pytest.mark.parametrize(
+    "test_input, expected_output",
+    [
+        (
+            pd.DataFrame(
+                data=[[0.0, 0.0], [0.0, 0.0], [0.0, 0.0]],
+                index=[pd.to_datetime('2000-01-01 01:00'),
+                        pd.to_datetime('2000-01-01 02:00'),
+                        pd.to_datetime('2000-01-01 03:00')],
+                columns=['A', 'B']
+            ),
+            pd.DataFrame(
+               data=[[0.0, 0.0]],
+               index=[datetime.date(2000, 1, 1)],
+               columns=['A', 'B']
+            )
+        ),
+        (
+            pd.DataFrame(
+                data=[[1, 2], [3, 4], [5, 6]],
+                index=[pd.to_datetime('2000-01-01 01:00'),
+                        pd.to_datetime('2000-01-01 02:00'),
+                        pd.to_datetime('2000-01-01 03:00')],
+                columns=['A', 'B'],
+            ),
+            pd.DataFrame(
+                data=[[5, 6]],
+                index=[datetime.date(2000, 1, 1)],
+                columns=['A', 'B']
+            )
+        ),
+    ])
+
+def test_daily_max(test_input, expected_output):
+    """Test mean function works for array of zeroes and positive integers."""
+    from catchment.models import daily_max
+    pdt.assert_frame_equal(daily_max(test_input), expected_output)
+
+# def test_daily_mean_zeros():
+#     """Test that mean function works for an array of zeros."""
+#     from catchment.models import daily_mean
+
+#     test_input = pd.DataFrame(
+#                      data=[[0.0, 0.0],
+#                            [0.0, 0.0],
+#                            [0.0, 0.0]],
+#                      index=[pd.to_datetime('2000-01-01 01:00'),
+#                             pd.to_datetime('2000-01-01 02:00'),
+#                             pd.to_datetime('2000-01-01 03:00')],
+#                      columns=['A', 'B']
+#     )
+#     test_result = pd.DataFrame(
+#                      data=[[0.0, 0.0]],
+#                      index=[datetime.date(2000, 1, 1)],
+#                      columns=['A', 'B']
+#     )
+
+#     # Need to use Pandas testing functions to compare arrays
+#     pdt.assert_frame_equal(daily_mean(test_input), test_result)
+
+# def test_daily_mean_integers():
+#     """Test that mean function works for an array of positive integers."""
+#     from catchment.models import daily_mean
+
+#     test_input = pd.DataFrame(
+#                      data=[[1, 2],
+#                            [3, 4],
+#                            [5, 6]],
+#                      index=[pd.to_datetime('2000-01-01 01:00'),
+#                             pd.to_datetime('2000-01-01 02:00'),
+#                             pd.to_datetime('2000-01-01 03:00')],
+#                      columns=['A', 'B']
+#     )
+#     test_result = pd.DataFrame(
+#                      data=[[3.0, 4.0]],
+#                      index=[datetime.date(2000, 1, 1)],
+#                      columns=['A', 'B']
+#     )
+
+    # # Need to use Pandas testing functions to compare arrays
+    # pdt.assert_frame_equal(daily_mean(test_input), test_result)
+
+# def test_daily_min_python_list():
+#     """Test for attributeError when passing a python list"""
+#     from catchment.models import daily_min
+
+#     with pytest.raises(AttributeError):
+#         error_expected = daily_min([[3, 4, 7], [-3, 0, 5]])
 
