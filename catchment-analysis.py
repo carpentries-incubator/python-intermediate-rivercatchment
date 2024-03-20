03a758ed7a6d60f4050020d52118eee56716a9c4
fontaine <Davefount@gmail.com>
2024-03-20 12:29:29 +0000

    updated analyse_data function

--- catchment-analysis.py
+++ catchment-analysis.py
@@ -19,7 +19,12 @@ def main(args):
         InFiles = [args.infiles]
     
     if args.full_data_analysis:
-        compute_data.analyse_data(os.path.dirname(InFiles[0]))
+        daily_standard_deviation = compute_data.analyse_data(os.path.dirname(InFiles[0]))
+
+        graph_data = {
+        'daily standard deviation': daily_standard_deviation
+        }
+        views.visualize(graph_data)
 
     for filename in InFiles:
         measurement_data = models.read_variable_from_csv(filename)
--- catchment/compute_data.py
+++ catchment/compute_data.py
@@ -20,15 +20,8 @@ def analyse_data(data_dir):
         raise ValueError('No CSV files found in the data directory')
     data = map(models.read_variable_from_csv, data_file_paths)
 
-    daily_std_list = []
-    for dataset in data:
-        daily_std = dataset.groupby(dataset.index.date).std()
-        daily_std_list.append(daily_std)
+    daily_std_list = map(lambda x: x.groupby(x.index.date).std(), data)
     
     daily_standard_deviation = pd.concat(daily_std_list)
 
-    graph_data = {
-        'daily standard deviation': daily_standard_deviation
-    }
-
-    views.visualize(graph_data)
+    return daily_standard_deviation
